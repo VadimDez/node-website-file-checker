0dd6161fc81c02490744a8fb042959b1701b8be8
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-10-15 20:04:31 +0200

    changes

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-const http = require('http');
+const https = require('https');
 const chalk = require('chalk');
 
 var sites = [];
-const length = sites.length;
+var found = [];
+var length = sites.length;
 
 function siteCallback(i) {
+  console.log(i);
   if (i >= length) {
     console.log(chalk.yellow('Finished!'));
+
+    if (found.length) {
+      console.log(chalk.yellow('Found:'));
+      found.forEach(function (site) {
+        console.log(chalk.yellow(site));
+      });
+    }
     return;
   }
+  console.log(chalk.grey('Checking ' + sites[i] + '...'));
   
-  var req = http.request({
+  var req = https.request({
     host: sites[i],
     path: '/.git/HEAD'
   }, function (res) {
     if (res.statusCode === 200) {
       console.log(chalk.green(sites[i]));
+      found.push(sites[i]);
     }
     siteCallback(i + 1);
   });
 
   req.on('error', function(e) {
     console.error(chalk.red(e));
+    siteCallback(i + 1);
   });
 
 
@@ -29,4 +41,5 @@ function siteCallback(i) {
 }
 
 console.log(chalk.yellow('Starting'));
+
 siteCallback(0);
